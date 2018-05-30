d646453ba7daa0f89a202292421f06471f392943
Eran Lunenfeld <eranl@paykey.com>
2018-05-30 11:21:29 +0300

    test app

---
+++ f/f.h
@@ -0,0 +1,19 @@
+//
+//  f.h
+//  f
+//
+//  Created by Eran Lunenfeld on 30/05/2018.
+//  Copyright © 2018 Eran Lunenfeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for f.
+FOUNDATION_EXPORT double fVersionNumber;
+
+//! Project version string for f.
+FOUNDATION_EXPORT const unsigned char fVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <f/PublicHeader.h>
+
+
